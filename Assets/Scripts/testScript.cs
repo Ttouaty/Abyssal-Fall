9747f00b6047af3d9b95dfbaa912774e6c010eb6
Ttouaty <t.touaty@student.isartdigital.com>
2016-04-11 18:55:57 +0200

    exemple pour les GDP 1

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour
+{
+	void Awake()
+	{ 
+	
+	}
+	
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void LateUpdate()
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision colli) 
+	{
+		if (colli.transform.tag == "Pouet")
+		{
+			Debug.Log("J'AI TOUCHÉ");
+		}
+
+	}
+}
