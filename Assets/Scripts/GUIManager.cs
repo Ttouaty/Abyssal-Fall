5bda3e2df9288bc3542ffaac63aa42f521e5db4a
Ttouaty <t.touaty@student.isartdigital.com>
2016-04-14 15:34:28 +0200

    Fix main scene

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,14 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour
+{
+	void Awake()
+	{
+		Transform myTransform = transform;
+		for (int c = 0; c < myTransform.childCount; ++c)
+		{
+			myTransform.GetChild(c).gameObject.SetActive(false);
+		}
+	}
+}
