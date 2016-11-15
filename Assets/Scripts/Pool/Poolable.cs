c92905ff5657884bc981134f2b84a96db5a308cc
Ttouaty <t.touaty@student.isartdigital.com>
2016-11-15 15:01:48 +0100

    Micro fix

--- Assets/Scripts/Pool/Poolable.cs
+++ Assets/Scripts/Pool/Poolable.cs
@@ -8,7 +8,6 @@ public class Poolable : MonoBehaviour
 	
 	public void AddToPool ()
     {
-        Pool.Reserve.Add(gameObject);
         gameObject.transform.parent = Pool.Root.transform;
         gameObject.transform.position = Vector3.zero;
         gameObject.transform.rotation = Quaternion.identity;
